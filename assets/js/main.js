6491c69983d69c7c0354ce93767726a363dcded1
Mohab Mohammed <92528415+mohabmohamed44@users.noreply.github.com>
2023-09-15 00:12:23 +0200

    Final Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,53 @@
-let inputs = document.querySelectorAll("input"); // Get all input elements on the page
-const errorMessage = document.querySelectorAll('.error-message'); // Get all error message elements on the page
+const inputs = document.querySelectorAll(".input input");
+const errorMessages = document.querySelectorAll('.input .error');
 
-inputs[0].addEventListener('input', function (event) { // Add an input event listener to the first input element
-    let value = event.target.value; // Get the value entered by the user
-    if (value < 30) { // If the value is less than 30
-        if (value == 0) { // If the value is 0, set it to 1
-            value = 1
+inputs[0].addEventListener('input', function (event) {
+    let value = event.target.value;
+    if (value < 30) {
+        if (value == 0) {
+            value = 1;
         }
-        value = value; // Keep the same value
-    } else if (value > 30) { // If the value is greater than 30
-        value = 30 // Set the value to 30
+        value = value;
+    } else if (value > 30) {
+        value = 30;
     }
-    event.target.value = value; // Set the input value to the corrected value
+    event.target.value = value;
 });
 
-inputs[1].addEventListener('input', function (event) { // Add an input event listener to the second input element
-    let value = event.target.value; // Get the value entered by the user
-    if (value == 0) { // If the value is 0, set it to 1
-        value = 1
+inputs[1].addEventListener('input', function (event) {
+    let value = event.target.value;
+    if (value == 0) {
+        value = 1;
     }
-    if (value < 12) { // If the value is less than 12
-        value = value; // Keep the same value
-    } else if (value > 12) { // If the value is greater than 12
-        value = 12 // Set the value to 12
+    if (value < 12) {
+        value = value;
+    } else if (value > 12) {
+        value = 12;
     }
-    event.target.value = value; // Set the input value to the corrected value
+    event.target.value = value;
 });
 
-inputs[2].addEventListener('input', function (event) { // Add an input event listener to the third input element
-    let value = event.target.value; // Get the value entered by the user
-    if (value == 0) { // If the value is 0, set it to 1
-        value = 1
+inputs[2].addEventListener('input', function (event) {
+    let value = event.target.value;
+    if (value == 0) {
+        value = 1;
     }
-    if (value < 2022) { // If the value is less than 2022
-        value = value; // Keep the same value
-    } else if (value > year) { // If the value is greater than 2022
-        value = year // Set the value to 2022
+    const currentYear = new Date().getFullYear();
+    if (value < 2022) {
+        value = value;
+    } else if (value > currentYear) {
+        value = currentYear;
     }
-    event.target.value = value; // Set the input value to the corrected value
+    event.target.value = value;
 });
 
-// Get the current date
 const now = new Date();
 let year = now.getFullYear();
-let month = now.getMonth() + 1; // add 1 because getMonth() is 0-indexed
+let month = now.getMonth() + 1;
 let day = now.getDate();
-console.log(year)
+console.log(year);
 
-function calculateAge(birthdate) { // Define a function to calculate the age based on a birthdate
+function calculateAge(birthdate) {
     const now = new Date();
     const birth = new Date(birthdate);
 
@@ -78,29 +78,29 @@ function calculateAge(birthdate) { // Define a function to calculate the age bas
     return age;
 }
 
-const years = document.querySelector("#years"); // Get the element with id "years"
-const months = document.querySelector("#months"); // Get the element with id "months"
-const days = document.querySelector("#days"); // Get the element with id "days"
-const button = document.getElementById("button"); // Get the button element with id "button"
+const years = document.querySelector("#years");
+const months = document.querySelector("#months");
+const days = document.querySelector("#days");
+const button = document.getElementById("button");
 
-button.addEventListener('click', function () { 
+button.addEventListener('click', function () {
     let isEmpty = false;
-    inputs.forEach(function (input) { 
+    inputs.forEach(function (input) {
         if (input.value.trim() === '') {
             isEmpty = true;
-            input.nextSibling.nextSibling.innerHTML = "please enter a number" 
-            input.style.border = "1px solid red"; 
+            input.nextElementSibling.innerHTML = "Please enter a number";
+            input.style.border = "1px solid red";
         }
     });
-    if (!isEmpty) { // If no input element is empty, calculate and display the age
+    if (!isEmpty) {
         for (let i = 0; i < inputs.length; i++) {
-            inputs[i].nextSibling.nextSibling.innerHTML = "" // Clear any error messages
-            inputs[i].style.border = "1px solid #ececec"; // Set the input element border back to its default color
+            inputs[i].nextElementSibling.innerHTML = "";
+            inputs[i].style.border = "1px solid #ececec";
         }
-        const birthdate = `${inputs[2].value}-${inputs[1].value}-${inputs[0].value}'`; // Get the birthdate from the input elements
-        const age = calculateAge(birthdate); // Calculate the age based on the birthdate
-        years.innerHTML = age.years; // Display the years
-        months.innerHTML = age.months; // Display the months
-        days.innerHTML = age.days; // Display the days
+        const birthdate = `${inputs[2].value}-${inputs[1].value}-${inputs[0].value}`;
+        const age = calculateAge(birthdate);
+        years.innerHTML = age.years;
+        months.innerHTML = age.months;
+        days.innerHTML = age.days;
     }
 });
